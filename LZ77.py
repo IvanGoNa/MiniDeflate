b2ecfd0dc796eb9ea4034017e1dae848cedf66f8
IvanGoNa <ivangomeznavas@gmail.com>
2025-10-28 18:55:38 +0100

    LZ77 compression algorithm done with few simple tests --> seems to work fine

---
+++ LZ77.py
@@ -0,0 +1,125 @@
+
+a= "ABAB"
+b= "ABCDEFABC"
+c= "ABABABABA"
+d= "AAAAAAA"
+e= "ABCDEFG"
+f= "ABABXABABY"
+
+def LZ77(string, lab_length, sb_length):
+
+    #info to compress
+    info = list(string)
+
+    #search_buffer of length 6
+    search_buffer=[]
+
+    #look ahead buffer of length 4
+    lab = info[0:lab_length] 
+
+    #contains the compressed info
+    compressed = []
+
+    n_byte = 0; 
+    while n_byte < len(info):
+        
+        c = info[n_byte]
+        p = 0
+        n = 0
+        
+        #If the byte c is not in the search buffer we return (0,0,c) if not, we continue
+        if c in search_buffer:
+            #We find the longest coincident sequence of the search buffer that imitates
+            #the beginning of the lab. 
+
+            #n -> length of that sequence (n <= len(lab))
+            #beginning -> where does the sequence start in the search buffer
+            beginning,n= lengthOfLongestCoincidentSequence(search_buffer, lab)
+
+            #p-> number of backwards jumps to do to get from the current byte to the byte were the sequence starts      
+            p = len(search_buffer)-beginning
+
+            #c-> new byte to add to the tuple, is "" if there is no more bytes to add
+            c = info[n_byte+n]  if (n_byte+n) < len(info) else ""
+
+        #We add to the search buffer those bytes present in the sequence found (if found)
+        for i in range(0, n+1):
+                if (n_byte + i) < len(info):
+                    search_buffer.append(info[n_byte+i])
+
+        #We update the byte number
+        n_byte += n + 1
+
+        #We update the search buffer (we get the last sb_length bytes)
+        search_buffer = search_buffer[-sb_length:]
+        #We update the lab (from the next byte to lab_length bytes ahead)
+        lab = info[n_byte: n_byte+lab_length]
+
+        tuple = (p,n,c)
+        compressed.append(tuple)
+
+    return compressed
+
+
+def lengthOfLongestCoincidentSequence(search_buffer, lab):
+    #Searches the longest coincident sequence between search_buffer and lab and returns its length
+    #and the beginning index. The sequence should be at the beggining of lab so if search_buffer = [a,b,c,d,e,f,g]
+    #and lab = [c,d,a,b,c,d], it returns 2.
+
+    maximum_length = 0
+    counter = 0
+    beginning_index = 0
+    for index, value in enumerate(search_buffer): 
+        #We start or continue counting if the counter is 0 and the value in search_buffer is
+        #the first of lab (we start counting a new sequence), or if the counter is between
+        #0 and the length of lab (we continue counting coincidences in a sequence).
+        if (counter == 0 and value == lab[0]) or isBetween(counter, 0, len(lab)):
+            if value == lab[counter]:
+                counter+=1
+            elif value == lab[0]:
+                counter=1
+            else:
+                counter=0
+
+            if counter >= maximum_length:
+                maximum_length = counter
+                beginning_index = index+1 - counter
+
+        #If the counter is bigger than the length of the look ahead buffer we cannot continue looking
+        #for coincidences, but is interesting for us to restart it to see if we can find a sequence with
+        #same length but a bit nearer to the lab.
+
+        if counter >= len(lab):
+            counter = 0;    
+
+    #If counter is not 0, we have found at least the possibility of a new sequence
+    #that started in the search_buffer but we can continue in the look_ahead_buffer
+    if counter > 0:
+        i = 0
+        while counter < len(lab) and counter != 0:
+            if lab[counter] == lab[i]:
+                i+=1
+                counter+=1
+                maximum_length = max(maximum_length, counter)
+            else:
+                counter = 0
+
+    return beginning_index, maximum_length
+
+
+def isBetween(a, b, c):
+    #returns true if an integrer a is between b and c
+    return (a > b and a < c)
+
+
+prueba1 = b"dalajdafdallajdalla"
+prueba2=  b"jdallajda"
+#print(lengthOfLongestCoincidentSequence(prueba1, prueba2))
+
+
+print(LZ77(a,4,6))
+print(LZ77(b,4,6)) 
+print(LZ77(c,4,6))
+print(LZ77(d,4,6))
+print(LZ77(e,4,6))
+print(LZ77(f,4,6))
